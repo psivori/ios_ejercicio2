7823aad6c2e0a34623a07c42000f0c5bca86a85f
Administrador <administrador@SP16.local>
2016-04-08 20:41:05 -0300

    switch funcionando

--- baseproject/EjercicioClase8/Classes/ViewController.swift
+++ baseproject/EjercicioClase8/Classes/ViewController.swift
@@ -14,18 +14,16 @@ class ViewController: UIViewController {
     @IBOutlet weak var profileName: UILabel!
     @IBOutlet weak var profileDescription: UITextView!
 
-    let users = [["name":"Walter White","image":"img-profile","description": "Walt es un padre de familia de 50 años, profesor de química en un instituto de Albuquerque, Nuevo México. Un día, Walt es diagnosticado con un cáncer de pulmón inoperable."],["name":"Jessie Pitman","image":"img-profile-2","description": "Walt es un padre de familia de 50 años, profesor de química en un instituto de Albuquerque, Nuevo México. Un día, Walt es diagnosticado con un cáncer de pulmón inoperable."],["name":"Tuco","image":"img-profile-4","description": "Walt es un padre de familia de 50 años, profesor de química en un instituto de Albuquerque, Nuevo México. Un día, Walt es diagnosticado con un cáncer de pulmón inoperable."],]
+    let users = [["name":"Walter White","image":"img-profile","description": "Walt es un padre de familia de 50 años, profesor de química en un instituto de Albuquerque, Nuevo México. Un día, Walt es diagnosticado con un cáncer de pulmón inoperable."],["name":"Jesse Pinkman","image":"img-profile-2","description": "Jesse Bruce Pinkman es el compañero de Walter White en el tráfico de metanfetamina. En el instituto, Jesse era un consumidor, fabricante y traficante de poca monta de metanfetamina. Era un estudiante que prestaba poca atención a las clases de química de su profesor, el propio Walter."],["name":"Skyler White","image":"img-profile-3","description": "Skyler White (de soltera Lambert) es la mujer de Walter White y madre de Walter White Jr. y Holly White. Skyler ha tenido múltiples trabajos para obtener dinero: escribiendo historias cortas, vendiendo objetos en eBay, trabajando como contable en Beneke Fabricators."],["name":"Tuco Salamanca","image":"img-profile-4","description": "Tuco Salamanca es un traficante de drogas mexicano que actuó brevemente como distribuidor de metanfetamina para Walter White y Jesse Pinkman. Tuco es un sociópata impredecible que a menudo se comporta de forma violenta."]]
 
     var currentUser : User!
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        if (currentUser == nil){
         changeProfile()
-        
-        profileImageView.image = UIImage(named: currentUser.image)
-        profileDescription.text = currentUser.description
-        profileName.text = currentUser.name
+        }
     }
 
     override func viewWillAppear(animated: Bool) {
@@ -40,7 +38,6 @@ class ViewController: UIViewController {
     
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
     }
 
     override func preferredStatusBarStyle() -> UIStatusBarStyle {
@@ -54,6 +51,9 @@ class ViewController: UIViewController {
     func changeProfile(){
         var i = Int.randomFrom(0, to: 3)
         currentUser = User.init(d: users[i])
+        profileImageView.image = UIImage(named: currentUser.image)
+        profileDescription.text = currentUser.description
+        profileName.text = currentUser.name
     }
 
     
